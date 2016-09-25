1a7ae9c2c062af2c1bf79281b6f257413cb34655
Kevin Hanna <kevinjackhanna@gmail.com>
2016-09-25 13:49:46 -0300

    Implement actions

--- App.elm
+++ App.elm
@@ -6,9 +6,10 @@ import Http
 import Json.Decode as Json
 import Task
 import Dict
+import Random
+import Array
 
-
-type Route = ListingAdventures | PlayingAdventure Adventure
+type Route = ListingAdventures | PlayingAdventure
 
 type alias AdventureLink =
   { title : String
@@ -18,7 +19,7 @@ type alias AdventureLink =
 type alias Adventure =
   { title : String
   , events: Dict.Dict String Event
-  , currentEvent: Maybe Event
+  -- , currentEvent: Maybe Event
   , startingEvent: Maybe Event
   }
 
@@ -31,6 +32,19 @@ type alias Event =
   { title: String
   -- , description: String
   , actions: List Action
+  }
+
+emptyEvent : Event
+emptyEvent =
+  { title = "Empty event"
+  , actions = []
+  }
+
+emptyAdventure : Adventure
+emptyAdventure =
+  { title = ""
+  , events = Dict.empty
+  , startingEvent = Just emptyEvent
   }
 
 main =
@@ -46,14 +60,21 @@ main =
 type alias Model =
   { adventureLinks : List AdventureLink
   , route : Route
+  , selectedAdventure : Maybe Adventure
+  , currentEvent : Maybe Event
   }
 
 init : (Model, Cmd Msg)
 init =
-  ( Model [] ListingAdventures
-  , getAdventureLinks
-  )
-
+  let
+    model =
+      { adventureLinks = []
+      , route = ListingAdventures
+      , selectedAdventure = Nothing
+      , currentEvent = Nothing
+      }
+  in
+    (model, getAdventureLinks)
 
 -- UPDATE
 type Msg
@@ -61,12 +82,18 @@ type Msg
   | FetchAdventureLinksSucceed (List AdventureLink)
   | FetchAdventureSucceed Adventure
   | FetchFail Http.Error
+  | RollAction Action
+  | SetEvent (Array.Array String) Int
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     FetchAdventureLinksSucceed adventureLinks ->
-      (Model adventureLinks ListingAdventures, Cmd.none)
+      let
+        newModel = { model | adventureLinks = adventureLinks }
+      in
+        (newModel, Cmd.none)
 
     FetchFail _ ->
       (model, Cmd.none)
@@ -76,22 +103,45 @@ update msg model =
 
     FetchAdventureSucceed adventure ->
       let
-        newModel = { model | route = PlayingAdventure adventure}
+        newModel = { model
+          | route = PlayingAdventure
+          , selectedAdventure = Just adventure
+          , currentEvent = adventure.startingEvent
+        }
       in
         (newModel, Cmd.none)
 
+    SetEvent eventIds randomIndex ->
+      case model.selectedAdventure of
+        Nothing -> (model, Cmd.none)
+        Just adventure ->
+          let
+            maybeEventId = Array.get randomIndex eventIds
+            eventId = Maybe.withDefault "4" maybeEventId
+            maybeEvent = Dict.get eventId adventure.events
+            newModel = { model | currentEvent = maybeEvent }
+          in
+            (newModel, Cmd.none)
+
+    RollAction action ->
+      let
+        array = Array.fromList action.outputs
+        length = Array.length array
+        randomIndex = Random.int 0 (length - 1)
+        cmd = Random.generate (SetEvent array) randomIndex
+      in
+        (model, cmd)
 
 -- VIEW
 view : Model -> Html Msg
 view model =
   case model.route of
     ListingAdventures -> viewAdventureLinks model.adventureLinks
-    PlayingAdventure adventure -> viewAdventure adventure
+    PlayingAdventure -> viewAdventure model.selectedAdventure model.currentEvent
 
 viewAdventureLink : AdventureLink -> Html Msg
 viewAdventureLink adventureLink = div
-  [
-    onClick (SelectAdventure adventureLink)
+  [ onClick (SelectAdventure adventureLink)
   ]
   [ text adventureLink.title
   ]
@@ -101,10 +151,20 @@ viewAdventureLinks adventureLinks =
   div []
     (List.map viewAdventureLink adventureLinks)
 
-viewAdventure : Adventure -> Html Msg
-viewAdventure adventure = div []
-  [ h1 [] [text adventure.title]
-  , viewEvent adventure.currentEvent
+viewAdventure : Maybe Adventure -> Maybe Event -> Html Msg
+viewAdventure maybeAdventure maybeCurrentEvent =
+  case maybeAdventure of
+    Nothing -> div [] []
+    Just adventure -> div []
+      [ h1 [] [text adventure.title]
+      , viewEvent maybeCurrentEvent
+      ]
+
+viewAction : Action -> Html Msg
+viewAction action = div
+  [ onClick (RollAction action)
+  ]
+  [ text action.description
   ]
 
 viewEvent : Maybe Event -> Html Msg
@@ -112,7 +172,7 @@ viewEvent maybe =
   case maybe of
     Just event -> div []
       [ text event.title
-      , div [] (List.map (\a -> text a.description) event.actions)
+      , div [] (List.map viewAction event.actions)
       ]
     Nothing -> text "Dead end."
 
@@ -150,10 +210,10 @@ decodeEvent = Json.object2 Event
 
 loadAdventure : String -> String -> (Dict.Dict String Event) -> Adventure
 loadAdventure title startingEventId events =
-  let
-    startingEvent = Dict.get startingEventId events
-  in
-    { title = title, startingEvent = startingEvent, events = events, currentEvent = startingEvent }
+  { title = title
+  , startingEvent = Dict.get startingEventId events
+  , events = events
+  }
 
 decodeAdventure : Json.Decoder Adventure
 decodeAdventure =
